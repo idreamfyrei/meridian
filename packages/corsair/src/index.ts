4267da1aefdd2d6f548d7e236f57f7e1adf1c177
Saumya <saumya.x3@gmail.com>
2026-06-10 01:07:23 +0530

    feat(corsair): add tenant scoped corsair client

--- packages/corsair/src/index.ts
+++ packages/corsair/src/index.ts
@@ -1,1 +1,30 @@
-export const corsairPackageName = "@meridian/corsair";
+import { gmail } from "@corsair-dev/gmail";
+import { googlecalendar } from "@corsair-dev/googlecalendar";
+import { createCorsair } from "corsair";
+import { Pool } from "pg";
+
+let pool: Pool | undefined;
+let corsair: ReturnType<typeof createCorsair> | undefined;
+
+function getCorsairPool(): Pool {
+  pool ??= new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  return pool;
+}
+
+export function getCorsair() {
+  corsair ??= createCorsair({
+    database: getCorsairPool(),
+    kek: process.env.CORSAIR_KEK ?? "",
+    multiTenancy: true,
+    plugins: [gmail(), googlecalendar()],
+  });
+
+  return corsair;
+}
+
+export function getTenantCorsair(workspaceId: string) {
+  return getCorsair().withTenant(workspaceId);
+}
